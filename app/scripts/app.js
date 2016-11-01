f99ebdef108686026b680ee439c23366eeb40b1f
haroldoolivieri <olivierisoares@gmail.com>
2016-11-01 18:19:38 -0200

    Inital commit using angular scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name crowdedInfoApp
+ * @description
+ * # crowdedInfoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('crowdedInfoApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crowdedInfoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the crowdedInfoApp
+ */
+angular.module('crowdedInfoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
