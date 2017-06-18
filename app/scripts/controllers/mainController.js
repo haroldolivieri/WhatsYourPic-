6769fcff45943c871e52147c404b06f631e53a9f
haroldoolivieri <olivierisoares@gmail.com>
2017-06-17 23:11:02 -0300

    saving on clarifai model

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -7,7 +7,7 @@
  * # MainCtrl
  * Controller of the whatsYourPic
  */
-whatsYourPic.controller('MainCtrl', function($rootScope, $scope, $window,
+whatsYourPic.controller('MainCtrl', function ($rootScope, $scope, $window,
     smoothScroll, localStorageService, $firebaseObject, $firebaseAuth,
     $moment, $firebaseArray, ngToast, ngProgressFactory, SweetAlert, $q, $http) {
 
@@ -17,32 +17,50 @@ whatsYourPic.controller('MainCtrl', function($rootScope, $scope, $window,
     var storageRef = firebase.storage().ref();
     var auth = $firebaseAuth();
 
-    auth.$signInAnonymously().then(function(firebaseUser) {
-        $scope.firebaseUser = firebaseUser;
-    }).catch(function(error) {
-        console.log(error);
-    });
 
-    //Save all facebook url photos in firebase storage
-    // var refForms = ref.child("crowdSourcing")
-    // var forms = $firebaseArray(refForms)
-    // getImages(forms)
-    // function getImages(forms){
-    //     $scope.forms = []
-    //     forms.$loaded().then(function() {
-    //         angular.forEach(forms, function(value, key) {
-    //             downloadImage(value.imageUrl).then(function(blob) {
-    //                 console.log(blob)
-    //                 console.log(value.$id)
-    //                 return uploadPhoto(blob, value.$id);
-    //             }).then(function(url) {
-    //                 console.log("saved! url:" + url)
-    //             }).catch(function(error) {
-    //                 console.log(error)
-    //             })
-    //         });
-    //     });
-    // }
+    var refForms = ref.child("crowdSourcing")
+    var forms = $firebaseArray(refForms)
+    //sendImagesToClarifai(forms)
+
+    function sendImagesToClarifai(forms) {
+        $scope.forms = []
+
+        auth.$signInAnonymously().then(function (firebaseUser) {
+            forms.$loaded().then(function () {
+
+                angular.forEach(forms, function (value, key) {
+                    getUrlFromFirebaseImage(value.$id).then(function (response) {
+                        return uploadPhotoToClarifai(response.url, response.uid);
+                    }).then(function (response) {
+                        return updateRow(response.uid, response.clarifaiId);
+                    }).then(function () {
+                        console.log("gravou no firebase");
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+                });
+            });
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }
+
+    var updateRow = function (uid, clarifaiId) {
+        var deferred = $q.defer();
+        console.log(uid)
+
+        ref.child("crowdSourcing").child(uid).on('value', function (snap) {
+            var data = snap.val();
+            data.sentToModel = true
+            data.clarifaiId = clarifaiId
+            ref.child("crowdSourcing").child(uid).update(data);
+            deferred.resolve();
+        }, function (err) {
+            deferred.reject(err);
+        });
+
+        return deferred.promise;
+    }
 
     $scope.form = {}
     $rootScope.selectedImage = "empty"
@@ -59,55 +77,55 @@ whatsYourPic.controller('MainCtrl', function($rootScope, $scope, $window,
         $rootScope.selectedButtonFacebook = true;
     }
 
-    $(document).on('fbload', function(){
+    $(document).on('fbload', function () {
         $rootScope.getFacebookPhotosIds()
     });
 
-    $rootScope.getFacebookPhotosIds = function() {
+    $rootScope.getFacebookPhotosIds = function () {
         FB.api("/" + $rootScope.facebookUserId +
-        "/photos?type=uploaded&limit=400&access_token=" +
-        $rootScope.facebookToken, function (response) {
-            if (!response || response.error) {
-                $rootScope.progressbar.complete();
+            "/photos?type=uploaded&limit=400&access_token=" +
+            $rootScope.facebookToken, function (response) {
+                if (!response || response.error) {
+                    $rootScope.progressbar.complete();
 
-                if (response.error.code == 190) {
-                    $rootScope.selectedButtonFacebook = false;
-                    $rootScope.facebookUserId = "";
-                    $rootScope.facebookToken = "";
-                    localStorageService.set('fbToken', $rootScope.facebookToken)
-                    localStorageService.set('fbUserId', $rootScope.facebookUserId)
-                    $rootScope.facebookLogin()
+                    if (response.error.code == 190) {
+                        $rootScope.selectedButtonFacebook = false;
+                        $rootScope.facebookUserId = "";
+                        $rootScope.facebookToken = "";
+                        localStorageService.set('fbToken', $rootScope.facebookToken)
+                        localStorageService.set('fbUserId', $rootScope.facebookUserId)
+                        $rootScope.facebookLogin()
+                    }
+                } else {
+                    $rootScope.selectedButtonFacebook = true;
+                    $rootScope.progressbar.set(40);
+                    getFacebookPhotosUrl(response);
                 }
-            } else {
-                $rootScope.selectedButtonFacebook = true;
-                $rootScope.progressbar.set(40);
-                getFacebookPhotosUrl(response);
-            }
-        });
+            });
     };
 
-    $rootScope.checkIfHasImages = function() {
+    $rootScope.checkIfHasImages = function () {
         if (!$rootScope.urlArray) {
             return false;
         }
         return $rootScope.urlArray.length > 0;
     }
 
-    $rootScope.checkIfHasImages = function() {
+    $rootScope.checkIfHasImages = function () {
         if (!$rootScope.photoArray) {
             return false;
         }
         return $rootScope.photoArray.length > 0;
     }
 
-    $scope.scrollToForm = function() {
+    $scope.scrollToForm = function () {
         var element = document.getElementById('form');
         smoothScroll(element);
         $scope.canShowFooter = false;
     }
 
-    $scope.selectImage = function(index) {
-        angular.forEach($rootScope.photoArray, function(value, key){
+    $scope.selectImage = function (index) {
+        angular.forEach($rootScope.photoArray, function (value, key) {
             value.selected = false;
             value.onHover = false;
         });
@@ -118,19 +136,19 @@ whatsYourPic.controller('MainCtrl', function($rootScope, $scope, $window,
         $scope.canShowFooter = true;
     }
 
-    $scope.$watch('form.date', function() {
+    $scope.$watch('form.date', function () {
         validateFields()
     });
 
-    $scope.$watch("form.location",function() {
+    $scope.$watch("form.location", function () {
         validateFields()
     });
 
-    $rootScope.$watch('selectedImage', function() {
+    $rootScope.$watch('selectedImage', function () {
         validateFields()
     });
 
-    var validateFields = function() {
+    var validateFields = function () {
         if ($scope.form.date && $scope.form.location && $rootScope.selectedImage != "empty") {
             $rootScope.validateSubmit = true;
         } else {
@@ -138,7 +156,7 @@ whatsYourPic.controller('MainCtrl', function($rootScope, $scope, $window,
         }
     }
 
-    $scope.sendForm = function() {
+    $scope.sendForm = function () {
 
         if ($scope.canClick == false) {
             return;
@@ -164,29 +182,37 @@ whatsYourPic.controller('MainCtrl', function($rootScope, $scope, $window,
         var forms = $firebaseArray(ref.child("crowdSourcing"))
 
         forms.$add({
-            location : getLocation(),
-            month : $moment($scope.form.date).format("MMMM"),
+            location: getLocation(),
+            month: $moment($scope.form.date).format("MMMM"),
             year: $moment($scope.form.date).format("YYYY"),
             imageUrl: $rootScope.selectedImage,
-            createdAt : ($moment().unix())*1000
-        }).then(function(ref) {
+            createdAt: ($moment().unix()) * 1000
+        }).then(function (ref) {
             $scope.id = ref.path.o[1]
             console.log("added record with id " + $scope.id);
             return downloadImage($rootScope.selectedImage);
-        }).then(function(blob) {
-            return uploadPhoto(blob, $scope.id);
-        }).then(function(url) {
-            console.log(url)
+        }).then(function (blob) {
+            return uploadPhotoToFirebaseStorage(blob, $scope.id);
+        }).then(function () {
+            return getUrlFromFirebaseImage($scope.id);
+        }).then(function (response) {
+            return uploadPhotoToClarifai(response.url, $scope.id);
+        }).then(function (response) {
+            console.log(response.clarifaiId)
+            return updateRow($scope.id, response.clarifaiId);
+        }).then(function (response) {
             SweetAlert.swal("Obrigado por participar!", "Informações enviadas " +
-            "com sucesso! Fique a vontade para enviar quantas desejar ;)", "success");
-        }).catch(function() {
-            SweetAlert.swal("Erro ao enviar sua foto, por favor, tente novamente", "error");
-        }).finally(function(){
+                "com sucesso! Fique a vontade para enviar quantas desejar ;)", "success");
+        }).catch(function (err) {
+            console.log(err)
+            SweetAlert.swal("Tivemos um problema :(",
+                "Houve um erro ao enviar sua foto, por favor, tente novamente", "error");
+        }).finally(function () {
             $scope.canClick = true
 
             $rootScope.selectedImage = "empty";
             $rootScope.selectedImageCheck = false;
-            angular.forEach($rootScope.photoArray, function(value, key){
+            angular.forEach($rootScope.photoArray, function (value, key) {
                 value.selected = false;
                 value.onHover = false;
             });
@@ -194,72 +220,112 @@ whatsYourPic.controller('MainCtrl', function($rootScope, $scope, $window,
         });
     }
 
-    var downloadImage = function(url){
+    var downloadImage = function (url) {
         var deferred = $q.defer();
-        $http.get(url, {responseType: "arraybuffer"}).success(function(data){
-            var arrayBufferView = new Uint8Array(data);
-            var blob = new Blob( [ arrayBufferView ], { type: "image/png" } );
+
+        $http({
+            method: 'GET',
+            url: url,
+            responseType: "arraybuffer"
+        }).then(function (res) {
+            let blob = new Blob([res.data], { type: 'image/png' });
             deferred.resolve(blob);
-        }).error(function(err, status){
+        }, function (error) {
             deferred.reject(err);
-        })
-        return deferred.promise;
-    }
-
-    var uploadPhoto = function upload(file, uid) {
-        var deferred = $q.defer();
-        var fileRef = storageRef.child("images").child(uid + ".png");
-        var uploadTask = fileRef.put(file);
-
-        uploadTask.on('state_changed', function(snapshot){
-            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            $rootScope.progressbar.set(progress);
-        }, function(error) {
-            deferred.reject(error.code);
-        }, function() {
-            console.log(uploadTask.snapshot)
-            deferred.resolve(uploadTask.snapshot.downloadURL);
         });
 
         return deferred.promise;
     }
 
-    var showCustomToast = function(message) {
+    var getUrlFromFirebaseImage = function (uid) {
+        console.log(uid)
+        var deferred = $q.defer();
+
+        auth.$signInAnonymously().then(function (firebaseUser) {
+            console.log(uid)
+            storageRef.child("images").child(uid + ".png").getDownloadURL().then(function (url) {
+                console.log(uid)
+                deferred.resolve({url : url, uid: uid});
+            }).catch(function (error) {
+                deferred.reject(error);
+            });
+        }).catch(function (error) {
+            deferred.reject(error);
+        });
+
+        return deferred.promise;
+    };
+
+    var uploadPhotoToClarifai = function (url, uid) {
+        var deferred = $q.defer();
+
+        console.log("CLaRIFAI")
+        app.inputs.create([{ url: url, id : uid, metadata: {firebaseId: uid, type: 'nostalgia'}}]).then(
+            function (response) {
+                console.log("CLaRIFAI 2")
+                deferred.resolve({clarifaiId : response[0].id, uid: uid});
+            },
+            function (err) {
+                console.log("CLaRIFAI 3")
+                deferred.reject(err);
+            });
+
+        return deferred.promise;
+    }
+
+    var uploadPhotoToFirebaseStorage = function (blob, uid) {
+        var deferred = $q.defer();
+        var fileRef = storageRef.child("images").child(uid + ".png");
+        var uploadTask = fileRef.put(blob);
+
+        uploadTask.on('state_changed', function (snapshot) {
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            $rootScope.progressbar.set(progress);
+        }, function (error) {
+            deferred.reject(error.code);
+        }, function () {
+            deferred.resolve();
+        });
+
+        return deferred.promise;
+    }
+
+    var showCustomToast = function (message) {
         ngToast.create({
             className: 'info',
             content: '<span class="">' + message + '</span>'
         });
     }
 
-    var getFacebookPhotosUrl = function(imageObjects) {
+    var getFacebookPhotosUrl = function (imageObjects) {
         $rootScope.photoArray = [];
         var count = imageObjects.data.length;
 
         var loadedCount = 0
-        angular.forEach(imageObjects.data, function(value, key){
+        angular.forEach(imageObjects.data, function (value, key) {
             FB.api("/" + value.id + "/picture?access_token=" +
-            $rootScope.facebookToken, function (response) {
-                if (response) {
-                    loadedCount ++;
-                    $rootScope.progressbar.set(40 + loadedCount*60/count);
+                $rootScope.facebookToken, function (response) {
+                    if (response) {
+                        loadedCount++;
+                        $rootScope.progressbar.set(40 + loadedCount * 60 / count);
 
-                    if (loadedCount == count) {
-                        $rootScope.progressbar.complete()
+                        if (loadedCount == count) {
+                            $rootScope.progressbar.complete()
+                        }
+
+                        var photo = {}
+                        photo.url = response.data.url
+                        photo.selected = false
+                        photo.onHover = false
+                        $rootScope.$apply(function () {
+                            $rootScope.photoArray.push(photo);
+                        });
                     }
-
-                    var photo = {}
-                    photo.url = response.data.url
-                    photo.selected = false
-                    photo.onHover = false
-                    $rootScope.$apply(function () {
-                        $rootScope.photoArray.push(photo);
-                    });
-                }
-            });
+                });
         });
     }
 
-    var getLocation = function() {
+    var getLocation = function () {
         var location = undefined
 
         if ($scope.form.location.formatted_address) {
